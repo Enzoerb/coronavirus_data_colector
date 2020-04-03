8a05c6413f61a513f1e7563a20a1bd522f13354a
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-04-03 09:55:22 -0300

    choose venv and arguments

--- create_crontab.py
+++ create_crontab.py
@@ -1,13 +1,29 @@
 
 from crontab import CronTab
 import os
+import sys
 import getpass
+
+
+def check_venv(arguments):
+    if len(arguments) >= 2:
+        if arguments[1] == 'local':
+            return os.path.join(actual_path, 'venv/bin/python3')
+        return arguments[1]
+    return os.path.join(actual_path, 'venv/bin/python3')
+
 
 username = getpass.getuser()
 actual_path = os.getcwd()
 python_file_path = os.path.join(actual_path, 'corona.py')
-venv_python_path = os.path.join(actual_path, 'venv/bin/python3')
+
+venv_python_path = check_venv(sys.argv)
+
 cron_command = f'{venv_python_path} {python_file_path}'
+
+for argument in sys.argv[2:]:
+    cron_command += f' {argument}'
+
 
 cron = CronTab(user=username)
 new_job = cron.new(command=cron_command)
